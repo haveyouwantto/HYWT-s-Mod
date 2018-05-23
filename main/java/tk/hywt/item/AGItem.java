f7cc94ccd69ddb5eaef271fcb413c01e193b4c4e
haveyouwantto <haveyouwantto@gmail.com>
2018-05-23 19:53:39 +0800

    Auto-generated Items

--- main/java/tk/hywt/item/AGItem.java
+++ main/java/tk/hywt/item/AGItem.java
@@ -1,6 +1,12 @@
 package tk.hywt.item;
 
 import net.minecraft.item.Item;
+import tk.hywt.lib.RefStrings;
+import tk.hywt.tab.HywtTabs;
 
 public class AGItem extends Item {
+	public static Item ags( String i ) 
+	{
+		return new Item().setUnlocalizedName(i).setCreativeTab(HywtTabs.MiscTab).setTextureName(RefStrings.modid+":"+i);
+	}
 }

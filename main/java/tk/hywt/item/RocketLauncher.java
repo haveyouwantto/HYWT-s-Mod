9af9943600afbc2d6b91d0ee869c6b0fbef33486
haveyouwantto <haveyouwantto@gmail.com>
2018-05-25 02:42:13 +0800

    Update Sounds and Langs
    Signed-off-by: haveyouwantto <haveyouwantto@gmail.com>

--- main/java/tk/hywt/item/RocketLauncher.java
+++ main/java/tk/hywt/item/RocketLauncher.java
@@ -18,7 +18,7 @@ public class RocketLauncher extends Item
         if (player.capabilities.isCreativeMode || player.inventory.consumeInventoryItem(Items.fire_charge))
         {
             //player.swingItem();
-            world.playSoundAtEntity(player, RefStrings.modid + ":rocket", 0.5F, 1.0F);
+            world.playSoundAtEntity(player, RefStrings.modid + ":cannon_fire", 0.5F, 1.0F);
             if (!world.isRemote)
             {
                 world.spawnEntityInWorld(new EntityRocket(world, player));
